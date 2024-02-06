9151bd1254c93aa38df151231439e5c70baec445
Tony Narlock <tony@git-pull.com>
2024-02-06 04:38:57 -0600

    libtmux.constants: Add OptionScope and OPTION_SCOPE_FLAG_MAP

--- src/libtmux/constants.py
+++ src/libtmux/constants.py
@@ -18,3 +18,28 @@ RESIZE_ADJUSTMENT_DIRECTION_FLAG_MAP: t.Dict[ResizeAdjustmentDirection, str] = {
     ResizeAdjustmentDirection.Left: "-L",
     ResizeAdjustmentDirection.Right: "-R",
 }
+
+
+class _DefaultOptionScope:
+    # Sentinel value for default scope
+    ...
+
+
+DEFAULT_OPTION_SCOPE: _DefaultOptionScope = _DefaultOptionScope()
+
+
+class OptionScope(enum.Enum):
+    """Scope used with ``set-option`` and ``show-option(s)`` commands."""
+
+    Server = "SERVER"
+    Session = "SESSION"
+    Window = "WINDOW"
+    Pane = "PANE"
+
+
+OPTION_SCOPE_FLAG_MAP: t.Dict[OptionScope, str] = {
+    OptionScope.Server: "-s",
+    OptionScope.Session: "",
+    OptionScope.Window: "-w",
+    OptionScope.Pane: "-p",
+}
